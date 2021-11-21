4ab7d6b19770931e2e513d2b1226a690cdea69b5
Harshitk-cp <harshitk.cp@gmail.com>
2021-11-21 16:35:32 +0530

    profile fragment changes

--- app/src/main/java/com/harshit/cafeshopapp/activity/fragments/ProfileFragment.java
+++ app/src/main/java/com/harshit/cafeshopapp/activity/fragments/ProfileFragment.java
@@ -5,12 +5,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.harshit.cafeshopapp.R;
+import com.harshit.cafeshopapp.activity.activity.DashboardActivity;
+import com.harshit.cafeshopapp.activity.model.FavModel;
+import com.harshit.cafeshopapp.activity.model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
@@ -31,7 +43,40 @@ public class ProfileFragment extends Fragment {
     txtProfileState = view.findViewById(R.id.txtProfileState);
     txtProfileZip = view.findViewById(R.id.txtProfileZip);
 
+    loadUserDataFromFirebase();
+    
     return view;
+
+  }
+
+  private void loadUserDataFromFirebase() {
+
+    FirebaseDatabase.getInstance().getReference("user").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+      .child("1")
+      .addValueEventListener(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+          if (snapshot.exists()) {
+            for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+              UserModel userModel = userSnapshot.getValue(UserModel.class);
+              txtProfileName.setText(new StringBuilder().append(userModel.getName()));
+              txtProfileAddress.setText(new StringBuilder().append(userModel.getAddress()));
+              txtProfileCity.setText(new StringBuilder().append(userModel.getCity()));
+              txtProfileState.setText(new StringBuilder().append(userModel.getState()));
+              txtProfileZip.setText(new StringBuilder().append(userModel.getZip()));
+            }
+          } else {
+            //Toast.makeText(DashboardActivity.this, "hah error", Toast.LENGTH_SHORT).show();
+          }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+          System.out.println("hah error");
+        }
+      });
+
 
   }
 }
